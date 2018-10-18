695431f893db5111af127bfb6314e476c0395481
TAEMJOKER <43587755+TAEMJOKER@users.noreply.github.com>
2018-10-18 19:55:07 +0300

    Update proc_msgs.lua

--- plugins/proc_msgs.lua
+++ plugins/proc_msgs.lua
@@ -1,6 +1,6 @@
 --[[
 << تم برمجه وكتابه السورس من قبل مطورين >>
-<<         @BLCON  \  @TH3BOSS          >>
+<<         @RRR_RP  \  @TH3LEDR        >>
 ]]
 local function pre_process(msg)
 --====================== Reply Only Group =====================================
